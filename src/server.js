68b2bfde22b3eb0a5708a83add77ef5d56a2420d
hung <hn6212040@gmail.com>
2025-12-28 16:34:30 +0700

    Update source code

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 // src/server.js
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const rootRouter = require('./routers/root.router');
-const prisma = require('./common/prisma/init.prisma');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const rootRouter = require("./routers/root.router");
+const prisma = require("./common/prisma/init.prisma");
 
 // 1. Import file Robot dọn dẹp
-const initCronJob = require('./common/cronjob'); 
+const initCronJob = require("./common/cronjob");
 
 dotenv.config();
 
@@ -18,29 +18,29 @@ app.use(cors());
 app.use(express.json());
 
 // Routes chính: Tất cả API sẽ bắt đầu bằng /api
-app.use('/api', rootRouter);
+app.use("/api", rootRouter);
 
 // Test Route
-app.get('/', (req, res) => {
-    res.send('BSS Backend is running...');
+app.get("/", (req, res) => {
+  res.send("BSS Backend is running...");
 });
 
 // Khởi động server
 const startServer = async () => {
-    try {
-        await prisma.$connect();
-        console.log('✅ Đã kết nối Database thành công!');
-        
-        // 2. Kích hoạt Robot chạy ngầm (Ngay sau khi kết nối DB thành công)
-        initCronJob(); 
-        
-        app.listen(PORT, () => {
-            console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error('❌ Không thể kết nối Database:', error);
-        process.exit(1);
-    }
+  try {
+    await prisma.$connect();
+    console.log("Đã kết nối Database thành công!");
+
+    // 2. Kích hoạt Robot chạy ngầm (Ngay sau khi kết nối DB thành công)
+    initCronJob();
+
+    app.listen(PORT, () => {
+      console.log(`Server đang chạy tại http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Không thể kết nối Database:", error);
+    process.exit(1);
+  }
 };
 
 startServer();
